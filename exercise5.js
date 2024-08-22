83fe86f10d1710c40820fdc1890053947783470b
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-08-22 09:30:22 +0200

    Add exercise 5

--- exercise5.js
+++ exercise5.js
@@ -1,7 +1,7 @@
-const calculateFactorial = (n) => {
-    if (n === 0 || n === 1) return 1;
+const calculateFactorial = (number) => {
+    if (number === 0 || number === 1) return 1;
     let factorial = 1;
-    for (let i = 2; i <= n; i++) {
+    for (let i = 2; i <= number; i++) {
         factorial *= i;
     }
     return factorial;
