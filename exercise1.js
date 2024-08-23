ec3dfde97596600f27efa40e8707757384d18801
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-08-23 17:53:48 +0200

    Update exercise 7

--- exercise1.js
+++ exercise1.js
@@ -1,7 +1,4 @@
 const calculateAverage = (numbers) => {
     const sum = numbers.reduce((sum, number) => sum += number, 0);
-    const count = numbers.length;
-    const average = sum / count;
-
-    return average;
+    return sum / numbers.length;
 }
