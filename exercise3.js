716aed37ec39ac8886700763bd632750af00956c
Albert Byiringiro <byiringiroalbert20@gmail.com>
2024-08-23 17:55:16 +0200

    Update exercise 3

--- exercise3.js
+++ exercise3.js
@@ -2,6 +2,3 @@ const reverseString = (string) => {
     if (typeof string !== "string") return "Error: Excepting strings only.";
     return string.split("").reverse().join("");
 };
-
-
-console.log(reverseString())
